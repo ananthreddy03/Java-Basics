c9b097a32701aa3c683d0d40b25e8b4175a43268
Ananth <ananthreddy@Bharaths-Air.domain.name>
2023-02-09 18:12:14 +0530

    Added Scanner Demo File

---
+++ Basics/src/com/ananth/ScannerDemo.java
@@ -0,0 +1,9 @@
+package com.ananth;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
