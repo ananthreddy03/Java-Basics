140ed8e9facc9e46540c6ef7655418c1fc226785
ananthreddy03 <apsreddy2003@gmail.com>
2023-02-09 18:18:00 +0530

    Added new line!

--- Basics/src/com/ananth/ScannerDemo.java
+++ Basics/src/com/ananth/ScannerDemo.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class ScannerDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Name :: ");
+        String name = scanner.next();
+        System.out.println("Name is :: " + name);
     }
 }
